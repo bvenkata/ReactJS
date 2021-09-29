ce9e1db6d6300e216ee6b551d3f3ab062410c390
Balaji Venkata <balaji.venkata.c@greenwayhealth.com>
2021-09-29 13:34:22 +0530

    updated  commit1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'; 
+import profile from './img/Profile.JPG';
+import rct from './img/ReactJs.png';
+import pyt from './img/Python.png';
+import prg from './img/Progress.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="ui fluid three item menu" style={{background: "black", color: "white"}}>
+        <a className="item" style={{color: "white"}}>About Me</a>
+        <a className="item" style={{color: "white"}}>Projects</a>
+        <a className="item" style={{color: "white"}}>Contact Details</a>
     </div>
+    <div style={{float: "left", paddingTop: 120, paddingLeft: 80}}>
+      <img className="ui medium circular image" src={profile}></img>
+     
+    </div>
+    
+    <div style={{float: "left", paddingTop: 100, paddingLeft: 80, fontFamily: 'ProximaNovaT' }}>
+    <p style={{fontSize: 45}}> About me. </p>
+
+    <p style={{fontSize: 20}}> Hi I am Balaji Venkatasubramaniyar, a 30 year old - Backend and Web developer and living in Bangalore, India.  </p>
+     
+    <p style={{fontSize: 20}}>  I am a Computer Science Engineer / Lead / Architect, currently working with JKT. </p>
+    
+   
+    <p style={{fontSize: 20}}> I am always excited to do business with like minded people. </p>
+    <br/>
+    <div style={{float: "left", fontFamily: 'ProximaNovaT', paddingTop:70 }}>
+    <p style={{fontSize: 30}}> Expert In - </p> 
+    </div>
+    <div style={{float: "left"}} className="ui small image">
+    <img src={rct}></img>
+    </div>
+    <div style={{float: "left"}} className="ui small image">
+    <img src={pyt}></img>
+    </div>
+    <div style={{float: "left"}} className="ui small image">
+    <img src={prg}></img>
+    </div>
+    </div>
+    </>
   );
 }
 
